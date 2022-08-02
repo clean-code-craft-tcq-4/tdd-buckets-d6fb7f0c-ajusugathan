1a5b1e47a1efd47597267368e89617f8494a8993
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-08-02 18:52:03 +0530

    Update detectRange.c

--- detectRange.c
+++ detectRange.c
@@ -1,2 +1,42 @@
 #include <stdio.h>
+#include "detectRange.h"
 
+int detectRange(int * reading,int size,rangeInfo *Data_struct)
+{
+    int totalRange=0;
+    Data_struct[totalRange].rangeStart=reading[0];
+	  Data_struct[totalRange].readingsInRange=1;
+    for(int i=0;i<size-1;i++)
+    {
+       if((reading[i]+1==reading[i+1])||(reading[i]==reading[i+1]))
+       {
+
+       Data_struct[totalRange].rangeEnd=reading[i+1];
+       Data_struct[totalRange].readingsInRange++;
+       }
+       else
+       {
+       totalRange++;
+       Data_struct[totalRange].rangeStart=reading[i+1];
+       Data_struct[totalRange].readingsInRange++;
+       }
+    }
+	  return(totalRange+1);
+}
+
+void printRangeInfo(rangeInfo *Data_struct,int totalRange)
+{
+    printf("\nRange         Count\n");
+    for(int i=0;i<totalRange;i++)
+    {
+         if(Data_struct[i].readingsInRange != 1)
+         {
+           printf("%d - %d , %d\n",Data_struct[i].rangeStart, Data_struct[i].rangeEnd,Data_struct[i].readingsInRange);
+         }
+         else
+         {
+            printf("%d , %d\n",Data_struct[i].rangeStart,Data_struct[i].readingsInRange); 
+         }
+    }
+
+}
