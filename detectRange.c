78ae62bb83c47bbae3d76f0b77d32e652f7098f8
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-08-02 19:14:44 +0530

    Update detectRange.c

--- detectRange.c
+++ detectRange.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include "detectRange.h"
 
-int detectRange(int * reading,int size,rangeInfo *Data_struct)
+int detectRange(int * readings,int size,rangeInfo *Data_struct)
 {
     int totalRange=0;
-    Data_struct[totalRange].rangeStart=reading[0];
-	  Data_struct[totalRange].readingsInRange=1;
-    for(int i=0;i<size-1;i++)
+    Data_struct[totalRange].rangeStart=readings[0];
+    Data_struct[totalRange].readingsInRange=1;
+    for(int Readcount=0;Readcount<size-1;Readcount++)
     {
-       if((reading[i]+1==reading[i+1])||(reading[i]==reading[i+1]))
+       if((readings[Readcount]+1==readings[Readcount+1])||(readings[Readcount]==readings[Readcount+1]))
        {
 
-       Data_struct[totalRange].rangeEnd=reading[i+1];
+       Data_struct[totalRange].rangeEnd=readings[Readcount+1];
        Data_struct[totalRange].readingsInRange++;
        }
        else
        {
        totalRange++;
-       Data_struct[totalRange].rangeStart=reading[i+1];
+       Data_struct[totalRange].rangeStart=readings[Readcount+1];
        Data_struct[totalRange].readingsInRange++;
        }
     }
@@ -27,15 +27,15 @@ int detectRange(int * reading,int size,rangeInfo *Data_struct)
 void printRangeInfo(rangeInfo *Data_struct,int totalRange)
 {
     printf("\nRange         Count\n");
-    for(int i=0;i<totalRange;i++)
+    for(int Rangecount=0;i<totalRange;Rangecount++)
     {
-         if(Data_struct[i].readingsInRange != 1)
+         if(Data_struct[Rangecount].readingsInRange != 1)
          {
-           printf("%d - %d , %d\n",Data_struct[i].rangeStart, Data_struct[i].rangeEnd,Data_struct[i].readingsInRange);
+           printf("%d - %d , %d\n",Data_struct[Rangecount].rangeStart, Data_struct[Rangecount].rangeEnd,Data_struct[Rangecount].readingsInRange);
          }
          else
          {
-            printf("%d , %d\n",Data_struct[i].rangeStart,Data_struct[i].readingsInRange); 
+            printf("%d , %d\n",Data_struct[Rangecount].rangeStart,Data_struct[Rangecount].readingsInRange); 
          }
     }
 
