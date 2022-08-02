ea074019347a303e3d268ee1e66637cba9b342aa
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-08-02 20:17:31 +0530

    Update testRange.c

--- testRange.c
+++ testRange.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
+#include "detectRange.h"
 
+void TEST_CASE_01()
+{
+    int data[2]={4,5};
+	  int totalreadings=sizeof(data)/sizeof(data[0]);
+    assert(DetectAndDisplayRange(data,totalreadings) == 1);
+}
 
 int main()
 {
-
+   TEST_CASE_01();
 }
