5eca77e6d6adcc25bf233b53af1b1884203541fb
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-08-02 18:50:27 +0530

    Update detectRange.h

--- detectRange.h
+++ detectRange.h
@@ -1,1 +1,10 @@
 
+typedef struct
+{
+int rangeStart;
+int rangeEnd;
+int readingsInRange;
+}rangeInfo;
+
+int detectRange(int * reading,int size,rangeInfo *Data_struct);
+void printRangeInfo(rangeInfo *Data_struct,int totalRange);
