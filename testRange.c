15df7237a8607dfa3d29c2e4d50d52f258928057
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-08-02 20:21:08 +0530

    Update testRange.c

--- testRange.c
+++ testRange.c
@@ -5,11 +5,17 @@
 void TEST_CASE_01()
 {
     int data[2]={4,5};
-	  int totalreadings=sizeof(data)/sizeof(data[0]);
+    int totalreadings=sizeof(data)/sizeof(data[0]);
     assert(DetectAndDisplayRange(data,totalreadings) == 1);
 }
-
+void TEST_CASE_02()
+{
+    int data[5]={4,4,5,8,10};
+    int totalreadings=sizeof(data)/sizeof(data[0]);
+    assert(DetectAndDisplayRange(data,totalreadings) == 3);
+}
 int main()
 {
+   TEST_CASE_01();
    TEST_CASE_01();
 }
